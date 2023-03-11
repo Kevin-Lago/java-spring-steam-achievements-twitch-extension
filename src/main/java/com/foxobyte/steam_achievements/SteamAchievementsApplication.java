0334d7efc7736372add7dbbe690744e947ddfcaa
Kevin Lago <kevinthelago@gmail.com>
2023-03-11 10:37:39 -0500

    Initial Commit

---
+++ src/main/java/com/foxobyte/steam_achievements/SteamAchievementsApplication.java
@@ -0,0 +1,13 @@
+package com.foxobyte.steam_achievements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamAchievementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamAchievementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foxobyte/steam_achievements/SteamAchievementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foxobyte.steam_achievements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamAchievementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
