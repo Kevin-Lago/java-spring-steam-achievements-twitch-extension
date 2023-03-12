2f93105ae5e619eb30dcad48254efbbd1b1ba0ab
Kevin Lago <kevinthelago@gmail.com>
2023-03-12 12:50:50 -0400

    Added Cross Origin

--- src/main/java/com/foxobyte/steam_achievements/controller/AchievementController.java
+++ src/main/java/com/foxobyte/steam_achievements/controller/AchievementController.java
@@ -3,6 +3,7 @@ package com.foxobyte.steam_achievements.controller;
 import com.foxobyte.steam_achievements.model.Achievement;
 import com.foxobyte.steam_achievements.service.AchievementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/achievements")
+@CrossOrigin
 public class AchievementController {
     @Autowired
     AchievementService service;
--- src/main/java/com/foxobyte/steam_achievements/controller/GameController.java
+++ src/main/java/com/foxobyte/steam_achievements/controller/GameController.java
@@ -4,6 +4,7 @@ import com.foxobyte.steam_achievements.client.steam.model.SteamGame;
 import com.foxobyte.steam_achievements.model.Game;
 import com.foxobyte.steam_achievements.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/games")
+@CrossOrigin
 public class GameController {
     @Autowired
     GameService service;
